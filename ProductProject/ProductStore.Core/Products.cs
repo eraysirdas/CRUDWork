520bafdccae3d1e10849a9ab3015ad6686eeef69
Eray Sirdas <sirdaseray.bjk@gmail.com>
2023-09-07 05:12:12 +0300

    7IN5D4-ERAY-SIRDAS

---
+++ ProductProject/ProductStore.Core/Products.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStore.Core
+{
+	public class Product
+	{
+		/*public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Price {  get; set; }
+        [Required]
+        public string Category { get; set; }
+
+        public string? Image {  get; set; } = string.Empty;
+        */
+		public int Id { get; set; }
+		[Required]
+		[MaxLength(50)]
+		public string ProductName { get; set; }
+		[Required]
+		public string Description { get; set; }
+		[Required]
+		public string Color { get; set; }
+		public string Category { get; set; }
+		public decimal Price { get; set; }
+	
+
+	
+	}
+}
---
+++ ProductProject/ProdutcStore/Controllers/ProductController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductStore.Core;
+using ProductStore.Infrastructure.Interfaces;
+
+namespace ProdutcStore.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepo;
+
+        public ProductController(IProductRepository productRepo)
+        {
+            _productRepo = productRepo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepo.GetAll();
+
+            return View(products);
+        }
+        [HttpGet]
+        public async Task <IActionResult> CreateOrEdit(int id= 0)
+        {
+            if(id==0)
+            {
+				return View(new Product());
+			}
+            else
+            {
+                try
+                {
+                    Product product = await _productRepo.GetById(id);
+                    if (product != null)
+                    {
+                        return View(product);
+                    }
+                }
+                catch (Exception ex )
+                {
+					TempData["errorMessage"] = ex.Message;
+					return RedirectToAction("Index");
+				}
+                TempData["errorMessage"] =$"Product details not found with Id : {id}";
+                return RedirectToAction("Index");
+            }
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrEdit(Product model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if(model.Id==0)
+                    {
+						await _productRepo.Add(model);
+						TempData["successMessage"] = "Product created successfully";
+						
+					}
+                    else
+                    {
+                        await _productRepo.Update(model);
+                        TempData["successMessage"] = "Product details updated successfully";
+                    }
+					return RedirectToAction(nameof(Index));
+				}
+                else
+                {
+                    TempData["errorMessage"] = "Model state is invalid";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+
+			}
+        }
+        [HttpGet]
+        public async Task<IActionResult>Delete (int id)
+        {
+            try
+            {
+                Product product = await _productRepo.GetById(id);
+                if (product != null)
+                {
+                    return View(product);
+                }
+            }
+            catch (Exception ex)
+            {
+				TempData["errorMessage"] =ex.Message;
+				return RedirectToAction("Index");
+			}
+			TempData["errorMessage"] = $"Product details not found with Id : {id}";
+			return RedirectToAction("Index");
+		}
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+                await _productRepo.Delete(id);
+                TempData["successMessage"] = "Product details updated successfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex )
+            {
+				TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+
+		}
+    }
+}
